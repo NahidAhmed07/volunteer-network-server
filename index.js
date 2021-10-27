b5abc807e56b5584d22b09f1c14152d4c63e4731
NahidAhmed07 <mdnahidahmed2002@gmail.com>
2021-10-27 09:35:19 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4v0cg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("volunteer_network");
+    const eventCollection = database.collection("events");
+
+    app.get("/events", async (req, res) => {
+      const cursor = eventCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    // await client.close()
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Volunteer Server Running");
+});
+
+app.listen(port, () => {
+  console.log("volunteer server running on port", port);
+});
